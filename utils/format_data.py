8a80627d9175e943b5983b2b518314f2b40981ac
Victoria Cheng <victoria.cheng15@gmail.com>
2024-04-28 12:01:33 -0600

    chore: add states and set city country for city and country columns

--- utils/format_data.py
+++
@@ -1,9 +0,0 @@
-def to_dict(id, place, mag, latitude, longitude, utc_time):
-    return {
-        "id": id,
-        "place": place,
-        "magnitude": mag,
-        "latitude": latitude,
-        "longitude": longitude,
-        "utc_time": utc_time,
-    }
