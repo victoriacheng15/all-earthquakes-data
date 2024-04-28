5062be54db642f1b07af6bbe6f108360539666b0
Victoria Cheng <victoria.cheng15@gmail.com>
2024-04-28 15:42:01 -0600

    refactor: edit postgres conn and change to code from id

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ def main():
     postgres = Postgres()
 
     try:
-        with postgres.connect() as connection:
-            logging.info(" ====> starting the process...")
-            print()
+        conn = postgres.connect()
+        logging.info(" ====> starting the process...")
+        print()
 
-            api_data = get_earthquakes_data()
-            db_ids = postgres.get_all_db_data(connection)
+        api_data = get_earthquakes_data()
+        db_ids = postgres.get_all_db_data()
 
-            for data in api_data:
-                if data["id"] not in db_ids:
-                    postgres.insert_data(connection, data)
+        for data in api_data:
+            if data["code"] not in db_ids:
+                postgres.insert_data(data)
 
-            print()
-            logging.info(" ====> the process is complete...")
-            print()
+        print()
+        logging.info(" ====> the process is complete...")
+        print()
 
     except Exception as e:
         logging.error(f"Error: {e}")
